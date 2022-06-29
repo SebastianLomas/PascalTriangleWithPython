ee0f88ff660be4ac0aa81ed3095ce11058c0a7d8
Sebastian Lomas <sebastianlomaslopez@hotmail.com>
2022-06-28 21:25:19 -0500

    I started the project

---
+++ main.py
@@ -0,0 +1,17 @@
+def existIndex(myList,index):
+    listLength = len(myList)
+
+    if(index <= listLength-1):
+        return True
+    else:
+        return False
+
+def main():
+    currentRow = [1]
+    nextRow = List()
+    previousNumber = 0
+
+
+
+if __name__ == '__main__':
+    main()

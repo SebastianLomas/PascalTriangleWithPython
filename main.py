675b7011353d13eeaa12403472f894ed20af3edb
Sebastian Lomas <sebastianlomaslopez@hotmail.com>
2022-07-04 11:43:19 -0500

    Input for number of rows now works

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ def indexExists(myList,index):
     else:
         return False
 
+def getNumberOfRows(message):
+    while True:
+        try:
+            numberOfRows = int(input(message))
+            return numberOfRows
+        except:
+            print("Error: The Value Most Be An Integer...")
+
+
 #You may ask why did i added this function instead of using spread operator
 #Well, when the pascal triangle is printend, numbers with more than two digits
 #ruin the format of the triangle, that's why i created it, I'll fix that.
@@ -19,9 +28,9 @@ def printPascal(rowToPrint):
         index += 1
 
 def main():
+    numberOfRows = getNumberOfRows("Give Me A Limit(Integer): ")
     currentRow = [1]
     nextRow = list()
-    numberOfRows = 10
     initialSpaces = numberOfRows - 1
     y = 0
 
@@ -46,6 +55,7 @@ def main():
         nextRow.clear()
         y += 1
         initialSpaces -= 1
+
 
 if __name__ == '__main__':
     main()
